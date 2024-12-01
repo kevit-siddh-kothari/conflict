62f6f82b9fb7b103cacbfd87727ad238fe8c84cd
siddh-kothari <siddh.kothari@kevit.io>
2024-12-01 09:53:05 +0530

    25

--- project-one/src/app/app.service.ts
+++ project-one/src/app/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const age = 12;
+    const age = 25;
     return 'Hello World!';
   }
 }
